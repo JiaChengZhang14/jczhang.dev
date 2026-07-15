243e8ce6f6649b2e61cb2e2a065267c5e72a2c6c
JiaCheng Zhang <jiachengmadrid@gmail.com>
2026-07-15 14:25:40 +0200

    fix: correct casing of projects folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,8 @@ import { Footer } from "@/components/Footer";
 import { AiAutomation } from "@/components/sections/ai-automation/AiAutomation";
 import { Experience } from "@/components/sections/experience/Experience";
 import { Hero } from "@/components/sections/hero/Hero";
-import { Projects } from "@/components/sections/projects/Projects";
 import { Skills } from "@/components/sections/skills/Skills";
-
+import { Projects } from "@/components/sections/projects/Projects";
 
 export default function Home() {
   return (
